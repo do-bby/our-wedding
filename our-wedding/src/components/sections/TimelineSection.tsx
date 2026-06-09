61cddcdd07480277655d45ae8b5268c04ce2df9b
yspark <yspark@ictpeople.co.kr>
2026-06-09 16:10:12 +0900

    타임라인 사진 추가

--- our-wedding/src/components/sections/TimelineSection.tsx
+++ our-wedding/src/components/sections/TimelineSection.tsx
@@ -1,6 +1,9 @@
 import { motion, useReducedMotion } from 'framer-motion'
 import './TimelineSection.css'
-import coverImage from '../../images/cover.png'
+import springImage from '../../images/spring.png'
+import summerImage from '../../images/summer.png'
+import fallImage from '../../images/fall.png'
+import winterImage from '../../images/winter.png'
 
 type TimelineSectionProps = {
   id?: string
@@ -22,27 +25,27 @@ export default function TimelineSection({ id = 'timeline' }: TimelineSectionProp
   const items = [
     {
       title: '2022년 10월',
-      subtitle: '처음 만난 날',
-      body: '서로의 하루에 조용히 스며든 첫 순간을 기억해요.',
-      image: coverImage,
+      subtitle: '가을',
+      body: '선선한 바람처럼 조용히 스며든 첫 순간을 기억해요.',
+      image: fallImage,
     },
     {
       title: '만난지 1주년',
-      subtitle: '',
-      body: '서로의 계절을 함께 걸으며 평범한 일상이 특별해졌어요.',
-      image: coverImage,
+      subtitle: '겨울',
+      body: '차가운 계절에도 서로의 온기가 되어 주며 더 가까워졌어요.',
+      image: winterImage,
     },
     {
       title: '만난지 2주년',
-      subtitle: '',
-      body: '우리의 하루가 쌓여 더 단단해졌고, 앞으로의 시간을 약속했어요.',
-      image: coverImage,
+      subtitle: '봄',
+      body: '새로움이 피어나는 계절처럼, 우리의 약속도 더 단단해졌어요.',
+      image: springImage,
     },
     {
       title: '만난지 3주년',
-      subtitle: '♥',
-      body: '이제 하나의 길을 걸어가려 합니다. 따뜻한 축복과 함께해 주세요.',
-      image: coverImage,
+      subtitle: '여름',
+      body: '환한 햇살 아래 이제 하나의 길을 걸어가려 합니다. 따뜻한 축복과 함께해 주세요.',
+      image: summerImage,
     },
   ]
 
